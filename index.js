bb05b8e380dbd0ae2dee8cf9d3432000d9cd8531
rwenite <rwenite@gmail.com>
2017-12-16 02:30:02 -0500

    Add middleware

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 // Richard Wen
 // rrwen.dev@gmail.com
 
+var mongoClient = require('mongodb').MongoClient
+var mongoQuerystring = require('mongo-querystring');
+
 /**
  * Description.
  *
  * @module express-mongodb-rest
- * @param {Object} [options={}] parameter description.
+ *
+ * @param {Object} req parameter description.
+ * @param {Object} res parameter description.
+ * @param {function} next parameter description.
+ *
  * @returns {Object} return description.
  *
  * @example
  * var expressmongodbrest = require('express-mongodb-rest');
  */
-module.exports = options => {
+module.exports = function(options) {
+	var out = {};
+	
+	// (options) Default options
 	options = options || {};
+	options.url = options.url || 'mongodb://localhost:27017';
+	options.database = options.database || 'test';
+	options.collection = options.collection || 'express_mongodb_rest';
+	
+	// (middleware) Express middleware function 
+	out.middleware = function(req, res, next) {
+		
+		// (middleware_parse) Parse url request to mongodb query
+		var query = mongoQuerystring.parse(req.query);
+		
+		// (middleware_query) Query mongodb database
+		mongoClient.connect(options.url, function(err, client) {
+			var collection = client.db(options.database).collection(options.collection);
+			if (err) next(err); // connect error
+			collection.find(query).toArray(function(err, docs) {
+				if (err) next(err); // query error
+				res.json(docs);
+			});
+		});
+	};
+	
+	// (app) Express built in app
+	out.app = {};
+	return(out);
 };
